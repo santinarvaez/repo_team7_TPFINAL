f34ca24ebad05b1af8fe098687e4dbd31ae52072
Santiago <Santiago@DucksPC>
2021-06-23 04:38:02 -0300

    Task: clases sin errores

--- ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/service/IProductLineService.java
+++ ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/service/IProductLineService.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.fi.tpfinal.service;
 
-public class IProductLineService {
+import java.util.List;
 
+import ar.edu.unju.fi.tpfinal.model.ProductLine;
+
+public interface IProductLineService {
+	public void addProductLine(ProductLine productLine);
+	public ProductLine getProductLine();
+	public List<ProductLine> getAllProductLines();
+	public ProductLine getProductLineForId(String id);
+	public void eliminarProductLine(String id);	
 }
