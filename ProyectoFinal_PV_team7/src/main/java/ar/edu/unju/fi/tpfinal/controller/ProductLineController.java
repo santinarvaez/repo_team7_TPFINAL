bda363f06f2f84398977575c0fea69c29cd2d94e
Santiago <Santiago@DucksPC>
2021-06-22 22:22:37 -0300

    Task: Repository classes and controller

--- ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/controller/ProductLineController.java
+++ ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/controller/ProductLineController.java
@@ -1,5 +1,10 @@
 package ar.edu.unju.fi.tpfinal.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ProductLineController {
 
+	
+	
 }
--- ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/repository/IProductLineRepository.java
+++ ProyectoFinal_PV_team7/src/main/java/ar/edu/unju/fi/tpfinal/repository/IProductLineRepository.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.tpfinal.repository;
+import org.springframework.data.repository.CrudRepository;
+import ar.edu.unju.fi.tpfinal.model.ProductLine;
 
-public interface IProductLineRepository {
-
+public interface IProductLineRepository extends CrudRepository<ProductLine,String> {
+	   
 }
